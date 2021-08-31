7c5788f904114dcfe964e1dfaba56fb9e91c2b1b
Nathan Braz <nathanbrz09@gmail.com>
2021-08-31 00:03:27 -0300

    Adicionando estilos globais da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import styled from 'styled-components';
+import { GlobalStyle } from "./styles/global";
 
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`;
 
 export function App() {
   return (
     <div className="App">
-      <Title>Hello World</Title>
+      <h1>Hello World</h1>
+      <GlobalStyle/>
     </div>
   );
 }
