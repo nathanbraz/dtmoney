650a3f12723684641c0d3dd559843311309282bc
Nathan Braz <nathanbrz09@gmail.com>
2021-09-02 14:51:07 -0300

    Adicionando a modal de Nova transaçao usando a biblioteca react-modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import logoImg from '../../assets/logo.svg';
+
 import { Container, Content } from './styles';
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+  
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>        
       </Content>
     </Container>
   );
