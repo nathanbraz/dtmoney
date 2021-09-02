bfeca9a1d142465f98ab209c09b54493c92a013b
Nathan Braz <nathanbrz09@gmail.com>
2021-09-02 15:37:01 -0300

    Adicionando formulario na modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './style';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -7,8 +8,16 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal ({ isOpen, onRequestClose}: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar transação</h2>
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose} overlayClassName="react-modal-overlay" className="react-modal-content">
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input type="text" placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Categoria" />
+        
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
