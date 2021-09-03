be521cb62c9b10d738b02bd91ec096f389f902b2
Nathan Braz <nathanbrz09@gmail.com>
2021-09-02 22:10:58 -0300

    Adicionando o acesso das transaçoes por meio de Contexto, refatorando o codigo e usando o contexto

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);  
+  
 
   function handleOpenNewTransactionModal(){
     setIsNewTransactionModalOpen(true);
@@ -20,11 +22,11 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
       <GlobalStyle/>
-    </>
+    </TransactionsProvider>
   );
 }
